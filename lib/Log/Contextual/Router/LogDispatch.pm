4cc1c0dd9d97909cd0281c38f6204504fd606512
Kent Fredric <kentfredric@gmail.com>
2013-12-14 20:22:21 +1300

    Build results of 0bdfafc (on master)

--- lib/Log/Contextual/Router/LogDispatch.pm
+++ lib/Log/Contextual/Router/LogDispatch.pm
@@ -18,6 +18,7 @@ extends 'Log::Contextual::Router';
 around handle_log_request => sub {
     my ( $orig, $self, %message_info ) = @_;
     require Carp;
+    ## no critic (ProhibitPackageVars)
     local $Carp::CarpLevel = $Carp::CarpLevel;
     $Carp::CarpLevel = $message_info{caller_level} + 1;
     return $self->$orig( %message_info );
